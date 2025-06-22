be97dba841f48a09dce22c0cfce1b4833f619b44
raviteja-112 <pangaraviteja.dev@gmail.com>
2025-06-22 11:09:19 +0530

    add vite implementation

---
+++ vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Page from "./page";
+
+function App() {
+  return (
+    <>
+      <Page />
+      <h1>Hello from app </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ vitereact/src/page.jsx
@@ -0,0 +1,5 @@
+function Page() {
+  return <h3>Hello from page app</h3>;
+}
+
+export default Page;
